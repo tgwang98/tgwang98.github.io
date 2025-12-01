34a06f355a59e7996c753308bfa984a3bfa8bb36
tgwang98 <96556431+tgwang98@users.noreply.github.com>
2025-12-01 14:29:28 -0800

    Change image class from 'object-cover' to 'object-contain'

--- src/components/publications/PublicationsList.tsx
+++ src/components/publications/PublicationsList.tsx
@@ -204,7 +204,7 @@ export default function PublicationsList({ config, publications, embedded = fals
                                                 src={`/papers/${pub.preview}`}
                                                 alt={pub.title}
                                                 fill
-                                                className="object-cover"
+                                                className="object-contain"
                                                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                                             />
                                         </div>
